eb10450764b2e7b33fc9ad0c855946738cf076a8
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-07 08:43:40 +0900

    :sparkles: 53. props で値を受け渡す

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { string } from 'prop-types'
+import { string, bool } from 'prop-types';
 
 function Hello(props) {
-  const { children } = props;
+  const { children, bang } = props;
   return (
     <View>
       <Text style={styles.text}>
-        {`Hello ${children}`}
+        {`Hello ${children}${bang ? '!' : ''}`}
       </Text>
     </View>
   );
@@ -15,7 +15,12 @@ function Hello(props) {
 
 Hello.propTypes = {
   children: string.isRequired,
-}
+  bang: bool,
+};
+
+Hello.defaultProps = {
+  bang: false,
+};
 
 const styles = StyleSheet.create({
   text: {
