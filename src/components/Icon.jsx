d6241a765c7fe7ee9aa5bb1b3f6e069fa6184ffc
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-08 08:39:23 +0900

    :sparkles: 65. カスタムアイコンを作成する - 2
    - アイコンをカスタムアイコンから読み込める機能を作成

---
+++ src/components/Icon.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+import { useFonts } from '@use-expo/font';
+import { number, string , oneOf} from 'prop-types';
+
+import icomoon from '../../assets/fonts/icomoon.ttf';
+import selection from '../../assets/fonts/selection.json';
+
+export default function Icon(props) {
+  const [fontLoaded] = useFonts({ icomoon });
+  const { name, size, color } = props;
+  const CustomIcon = createIconSetFromIcoMoon(selection);
+  // アイコンがすべて読み込まれるまで待つための処理
+  // この処理がないと読み込まれていないのでエラーになってしまう
+  if (!fontLoaded) {
+    return null;
+  }
+  // style={{ lineHeight: size - 1 }} に関しては Android でボタンの位置がずれてしまう対応
+  return <CustomIcon name={name} size={size} color={color} style={{ lineHeight: size - 1 }} />;
+}
+Icon.propTypes = {
+  name: oneOf(['plus', 'delete', 'pencil', 'check']).isRequired,
+  size: number,
+  color: string,
+};
+
+Icon.defaultProps = {
+  size: 24,
+  color: '#000',
+};
