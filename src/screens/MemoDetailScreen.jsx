d737a1e012276ef56e44593ee495480f44b27727
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-08 07:57:54 +0900

    :sparkles: 64. いろいろなアイコンを表示する
    - expo/vector-icons のインストール
    - CircleButtonコンポーネントをアイコン化
    - npm から yarn に変更

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import {
+  View, Text, ScrollView, StyleSheet,
+} from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
