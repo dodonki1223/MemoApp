65aef45046e366c3407f717ca34da0e7d43a0004
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-07 09:49:13 +0900

    :sparkles: 59. コードをコンポーネントとして抽出する - 2
    - MemoList をコンポーネント化

--- App.jsx
+++ App.jsx
@@ -2,44 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import AppBar from './src/components/AppBar';
+import MemoList from './src/components/MemoList';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
-
-      <View>
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-      </View>
-
+      <MemoList />
       <View style={styles.circleBotton}>
         <Text style={styles.circleBottonLabel}>+</Text>
       </View>
@@ -51,25 +20,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  memoListItem: {
-    backgroundColor: '#fff',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0, 0, 0, 0.15)',
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32,
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484',
   },
   circleBotton: {
     backgroundColor: '#467FD3',
