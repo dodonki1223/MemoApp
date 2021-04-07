d0cf44e4da3ebea276cfb1cc4d702bfbb494a260
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-08 08:58:20 +0900

    :sparkles: 67. KeyboardAvoidingView でテキスト領域を調整
    # ======== Emoji Prefix ========
    # 🎉  :tada: Initial commit.
    # ✨  :sparkles: New features.
    # 🔧  :wrench: Changing configuration files.
    # 💄  :lipstick: Updating the UI and style files.
    # ✏  :pencil2: Fixing typos.
    # 📝  :memo: Writing docs.
    # 🔥  :fire: Removing code or files.
    # 🐛  :bug: Fixing a bug.
    # ♻  :recycle: Refactoring code.
    # 🚨  :rotating_light: Removing linter warnings.
    # 🚧  :construction: Work in progress.
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # for http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import {
+  View, TextInput, StyleSheet, KeyboardAvoidingView,
+} from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
 
 export default function MemoEditScreen() {
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container} behavior="height" >
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="買い物リスト" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
