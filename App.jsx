d54b67b68c8dcc03b338bf21c17eb1be2230c417
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-07 09:46:16 +0900

    :sparkles: 59. コードをコンポーネントとして抽出する - 1
    - AppBar をコンポーネント化

--- App.jsx
+++ App.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AppBar from './src/components/AppBar';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.appbar}>
-        <View style={styles.appbarInner}>
-          <Text style={styles.appbarTitle}>Memo App</Text>
-          <Text style={styles.appbarRight}>ログアウト</Text>
-        </View>
-      </View>
+      <AppBar />
 
       <View>
         <View style={styles.memoListItem}>
@@ -54,28 +51,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F0F4F8',
-  },
-  appbar: {
-    width: '100%',
-    height: 104,
-    backgroundColor: '#467FD3',
-    justifyContent: 'flex-end',
-  },
-  appbarInner: {
-    alignItems: 'center',
-  },
-  appbarRight: {
-    position: 'absolute',
-    right: 19,
-    bottom: 16,
-    color: 'rgba(255, 255, 255, 0.8)',
-  },
-  appbarTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    color: '#fff',
-    fontWeight: 'bold',
   },
   memoListItem: {
     backgroundColor: '#fff',
