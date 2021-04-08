c9b685ce070bb23eebbe07b2db063f143d550a82
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-09 06:38:18 +0900

    :sparkles: 71. ログイン画面のスタイリング

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -9,16 +9,16 @@ export default function LogInScreen() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <View>
-        <Text>Log In</Text>
-        <TextInput value="Email Address" />
-        <TextInput value="Password" />
-        <View>
-          <Text>Submit</Text>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Log In</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonLabel}>Submit</Text>
         </View>
-        <View>
-          <Text>Not registered?</Text>
-          <Text>Sign up here!</Text>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
         </View>
       </View>
     </View>
@@ -27,5 +27,53 @@ export default function LogInScreen() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#F0F4F8',
+  },
+  inner: {
+    paddingHorizontal: 27,
+    paddingVertical: 24,
+  },
+  title: {
+    fontSize: 24,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    marginBottom: 24,
+  },
+  input: {
+    fontSize: 16,
+    height: 48,
+    // lineHeight: 32, // lineHeight を指定すると iPhone だと文字がちょっと下よりに表示されるため今回は削除する
+    borderColor: '#000',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  buttonContainer: {
+    backgroundColor: '#467FD3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    paddingVertical: 8,
+    paddingHorizontal: 32,
+    color: '#fff',
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467FD3',
+  },
+  footer: {
+    flexDirection: 'row',
   },
 });
