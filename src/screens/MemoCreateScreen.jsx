da37f00a4e5e25c91139592cbc90433b78382c76
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-09 06:04:25 +0900

    :sparkles: 69. KeyboardAvoidingView のバグ
    - 自作のコンポーネント KeyboardSafeView で対応する

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView,
-} from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import CircleButton from '../components/CircleButton';
+import KeyboardSafeView from '../components/KeyboardSafeView';
 
 export default function MemoCreateScreen() {
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="height" >
+    // <KeyboardAvoidingView style={styles.container} behavior="height" >
+    // 本来なら KeyboardAvoidingView を使用すべきだが、バグがありスタンプに切り替えたり
+    // すると CircleButton が隠れてしまうため、自作の component を使用する
+    <KeyboardSafeView style={styles.container}>
       <AppBar />
       <View style={styles.inputContainer}>
         <TextInput value="" multiline style={styles.input} />
       </View>
       <CircleButton name="check" />
-    </KeyboardAvoidingView>
+    </KeyboardSafeView>
   );
 }
 
