78983f2aee058f88b937b37b89cbbf7cd5f92282
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-09 07:00:05 +0900

    :sparkles: 73. ユーザーのアクションを受け取る

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { string, shape } from 'prop-types';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { string, shape, func } from 'prop-types';
 
 import Icon from './Icon';
 
 // NOTE: props の引数部分は { style, name } で分割代入で代わりができる
 export default function CircleButton(props) {
-  const { style, name } = props;
+  const { style, name, onPress } = props;
   return (
-    <View style={[styles.circleButton, style]}>
+    <TouchableOpacity style={[styles.circleButton, style]} onPress={onPress}>
       <Icon name={name} size={40} color="white" />
-    </View>
+    </TouchableOpacity>
   );
 }
 
 CircleButton.propTypes = {
   style: shape(), // shape は何でも受け取れる奴
   name: string.isRequired,
+  onPress: func,
 };
 
 CircleButton.defaultProps = {
   style: null,
+  onPress: null,
 };
 
 const styles = StyleSheet.create({
