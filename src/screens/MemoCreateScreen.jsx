9d3410b89f82184e153174db620e03a1d6668a1f
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-21 09:28:11 +0900

    :sparkles: 90. データベースからデータを取得する - 2
    - 変数名のバグを修正
    - orderBy メソッドで存在しないカラムを指定すると１件も取得できなくなる。そもそもエラーがでない

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -16,7 +16,7 @@ export default function MemoCreateScreen(props) {
     const ref = db.collection(`users/${currentUser.uid}/memos`);
     ref.add({
       bodyText,
-      updateAt: new Date(),
+      updatedAt: new Date(),
     })
       .then((docRef) => {
         console.log('Created!', docRef.id);
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Alert } from 'react-native';
 import firebase from 'firebase';
 
@@ -8,6 +8,7 @@ import LogOutButton from '../components/LogOutButton';
 
 export default function App(props) {
   const { navigation } = props;
+  const [memos, setMemos] = useState([]);
 
   useEffect(() => {
     navigation.setOptions({
@@ -26,12 +27,20 @@ export default function App(props) {
       // onSnapshot のメソッドでデータの取得に失敗することがあるのでその処理も記述する
       // ネットワークの問題などにより失敗する可能性がある
       unsubscribe = ref.onSnapshot((snapshot) => {
+        const userMemos = [];
         snapshot.forEach((doc) => {
-          console.log(doc.id, doc.data());
-        }, (error) => {
-          console.log(error);
-          Alert.alert('データの読み込みに失敗しました。');
+          const data = doc.data();
+          userMemos.push({
+            id: doc.id,
+            bodyText: data.bodyText,
+            updatedAt: data.updatedAt.toDate(),
+          });
         });
+        setMemos(userMemos);
+      }, (error) => {
+        console.log(error);
+        Alert.alert('データの読み込みに失敗しました。');
+      });
       });
     }
     return unsubscribe;
