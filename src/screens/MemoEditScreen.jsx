b8cf90300d20ca71118cf501074d2c2e978d9094
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-04-23 09:31:03 +0900

    :sparkles: 95. メモの編集画面を実装する - 2
    - 編集機能を実装
    - firestore に編集した内容が更新される機能を追加

--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import {
+  View, TextInput, StyleSheet, Alert,
+} from 'react-native';
 import { shape, string } from 'prop-types';
+import firebase from 'firebase';
 
 import CircleButton from '../components/CircleButton';
 import KeyboardSafeView from '../components/KeyboardSafeView';
@@ -9,18 +12,48 @@ export default function MemoEditScreen(props) {
   const { navigation, route } = props;
   const { id, bodyText } = route.params;
   const [body, setBody] = useState(bodyText);
+
+  function handlePress() {
+    const { currentUser } = firebase.auth();
+    if (currentUser) {
+      const db = firebase.firestore();
+      const ref = db.collection(`users/${currentUser.uid}/memos`).doc(id);
+      /*
+          ref.set()
+            ref.set() を使用すると引数で設定した Object で更新される
+            なので本来持っていた Object の key と Value を指定しないと
+            消えてしまうので注意が必要
+          ref.set({}, { merge: true })
+            ref.set({}, { merge: true })を使用すると既存の値を持ったまま
+            更新してくれる。第一引数にすべての値をセットできない状態なら
+            { merge: true } をセットしておくとよい
+       */
+      ref.set({
+        bodyText: body,
+        updatedAt: new Date(),
+      }, { merge: true })
+        .then(() => {
+          navigation.goBack();
+        })
+        .catch((error) => {
+          Alert.alert(error.code);
+        });
+    }
+  }
+
   return (
     <KeyboardSafeView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
-          value="買い物リスト"
+          value={body}
           multiline
           style={styles.input}
+          onChangeText={(text) => { setBody(text); }}
         />
       </View>
       <CircleButton
         name="check"
-        onPress={() => { navigation.goBack(); }}
+        onPress={handlePress}
       />
     </KeyboardSafeView>
   );
