96b8e5006075cedc80d5bbe20d3e9f5065b0d71a
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2021-05-10 08:49:20 +0900

    :sparkles: 98. ローディングコンポーネントでUX改善

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -5,11 +5,13 @@ import {
 import firebase from 'firebase';
 
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   /*
     useEffect(callback)
@@ -36,12 +38,16 @@ export default function LogInScreen(props) {
       }, []);
   */
   useEffect(() => {
+    // isLoading のデフォルトを true に設定しているため、 if が true の時は Loading が表示され
+    // ログインしていなかったら false を設定するため、何も表示されなくなる
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         navigation.reset({
           index: 0,
           routes: [{ name: 'MemoList' }],
         });
+      } else {
+        setLoading(false);
       }
     });
     // LoginScreen がアンマウントされる直前に実行され、
@@ -51,6 +57,7 @@ export default function LogInScreen(props) {
   }, []);
 
   function handlePress() {
+    setLoading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const { user } = userCredential;
@@ -62,11 +69,15 @@ export default function LogInScreen(props) {
       })
       .catch((error) => {
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
   }
 
   return (
     <View style={styles.container}>
+      <Loading isLoading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput
